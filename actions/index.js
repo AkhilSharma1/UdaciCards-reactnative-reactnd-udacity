8a59e3bc7757f2aecfb697616e63fa58bb72bb81
Akhil Sharma <akhil.sharma0910@gmail.com>
2018-09-17 15:46:33 -0400

    Redux Actions

---
+++ actions/index.js
@@ -0,0 +1,20 @@
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+export const ADD_DECK = 'ADD_DECK'
+
+export function receiveDecks(decks){
+    return {
+        type: RECEIVE_DECKS,
+        decks
+    }
+}
+
+export function addDeck(title){
+    return {
+        type: ADD_DECK,
+        title
+    }
+}
+
+export function handleAddDeck(title){
+    return (dispatch)
+}

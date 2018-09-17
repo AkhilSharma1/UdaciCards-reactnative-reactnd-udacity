c4dc51607eceb9258ba06592b0788d458714d94b
Akhil Sharma <akhil.sharma0910@gmail.com>
2018-09-17 15:57:18 -0400

    Fix Redux actions

--- actions/index.js
+++ actions/index.js
@@ -1,20 +1,42 @@
-export const RECEIVE_DECKS = 'RECEIVE_DECKS'
-export const ADD_DECK = 'ADD_DECK'
+export const ADD_CARD = "ADD_CARD";
+export const RECEIVE_DECKS = "RECEIVE_DECKS";
+export const ADD_DECK = "ADD_DECK";
 
-export function receiveDecks(decks){
-    return {
-        type: RECEIVE_DECKS,
-        decks
-    }
+import { addCardToDeck } from "../utils/api";
+import { saveDeckTitle } from "../utils/api";
+
+export function receiveDecks(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks
+  };
 }
 
-export function addDeck(title){
-    return {
-        type: ADD_DECK,
-        title
-    }
+export function handleAddDeck(title) {
+  return dispatch => {
+    return saveDeckTitle(title).then(() => dispatch(addDeck(title)));
+  };
 }
 
-export function handleAddDeck(title){
-    return (dispatch)
+function addDeck(title) {
+  return {
+    type: ADD_DECK,
+    title
+  };
+}
+
+export function handleAddCard(title, card) {
+  return dispatch => {
+    return addCardToDeck(title, card).then(() =>
+      dispatch(addCard(title, card))
+    );
+  };
+}
+
+function addCard(title, card) {
+  return {
+    type: ADD_CARD,
+    title,
+    card
+  };
 }

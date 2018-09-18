ace4837ee5166650a590eb403101ad4e99b02ece
Akhil Sharma <akhil.sharma0910@gmail.com>
2018-09-18 14:48:08 -0400

    Bind Redux and yarn upgrade

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStore } from "redux";
 import reducer from "./reducers/index";
 import {Provider} from 'react-redux';
 import middleware from "./middleware/index";
+import DeckList from "./components/DeckList";
 
 
 export default class App extends React.Component {
@@ -17,9 +18,7 @@ export default class App extends React.Component {
     return (
       <Provider store = {store}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <DeckList/>
       </View>
       </Provider>
     );
